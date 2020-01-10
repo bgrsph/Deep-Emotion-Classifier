74f2ed2d5dd66a961826bbeed6d0b754a0e13d1f
Orçun Özdemir <44787039+OzdemirOrcun@users.noreply.github.com>
2020-01-10 19:15:07 +0300

    Final Adjustment

---
+++ domain/models/model.py
@@ -0,0 +1,50 @@
+class Model(BaseNetwork):
+    def _init_(self, channels=1):
+        super(Model,self)._init_('iw' + str(channels), channels)
+
+        self.features = nn.Sequential(
+            # Block 1
+            nn.Conv2d(in_channels=12 * channels, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=2, stride=2),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(inplace=True),
+
+            # Block 2
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=2, stride=2),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(inplace=True),
+
+            nn.Conv2d(in_channels=128, out_channels=1, kernel_size=1, stride=1),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(1 * 16 * 16, 128),
+            nn.LeakyReLU(inplace=True),
+            nn.Dropout(0.5, inplace=True),
+
+            nn.Linear(128, 64),
+            nn.LeakyReLU(inplace=True),
+            nn.Dropout(0.5, inplace=True),
+
+            nn.Linear(64, 4),
+        )
+
+        self.initialize_weights()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        x = F.log_softmax(x, dim=1)
+        return x
